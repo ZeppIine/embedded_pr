b73beaf4179ce59ce745b4251d7aff493bf81797
ZeppIine <kimmin0904@gmail.com>
2023-11-10 14:54:35 +0900

    add dcmotor

---
+++ pr7/pr7_dcmt.c
@@ -0,0 +1,45 @@
+#include <wiringPi.h>
+#include <stdio.h>
+#include <stdlib.h> /* atoi( ) */
+
+#define PWM0 18
+#define PWM1 19
+
+void motorControl(int speed, int dir)
+{
+    if (dir)
+    {
+        pwmWrite(PWM1, 0);
+        pwmWrite(PWM0, speed);
+    }
+    else
+    {
+        pwmWrite(PWM0, 0);
+        pwmWrite(PWM1, speed);
+    }
+}
+
+int main(void)
+{
+    printf("DC Motor PWM Example with wiringPi\n");
+    wiringPiSetupGpio();
+    int speed = 0;
+    int dir = 0;
+    int range = 100;
+    int divisor = 192;
+
+    pinMode(PWM0, PWM_OUTPUT);
+    pinMode(PWM1, PWM_OUTPUT);
+    pwmWrite(PWM1, 0);
+    pwmWrite(PWM0, 0);
+    pwmSetMode(PWM_MODE_MS);
+    pwmSetRange(range);
+    pwmSetClock(divisor);
+
+    while (1)
+    {
+        scanf("%d %d", &speed, &dir);
+        motorControl(speed, dir);
+    }
+    return 0;
+}
