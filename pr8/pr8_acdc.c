c463aaa90ab53b77946999d47af566107e4b90be
ZeppIine <kimmin0904@gmail.com>
2023-11-17 14:49:07 +0900

    modify pr8 acdc

--- pr8/pr8_acdc.c
+++ pr8/pr8_acdc.c
@@ -6,12 +6,12 @@
 static const char *I2C_DEV = "/dev/i2c-1"; // I2C
 int main()
 {
-    int i2c_fd;          // i2c
-    int cnt = 0;         //
-    int preVal = 0;      //
-    int curVal = 0;      //
-    int threshold = 150; //
-    int adcChannel = 0;  //
+    int i2c_fd;                    // i2c
+    int cnt = 0;                   //
+    int preVal = 0;                //
+    int curVal = 0;                //
+    int threshold = 150;           //
+    int adcChannel[3] = {0, 1, 3}; //
     // wiringPi
     if (wiringPiSetupGpio() < 0)
     {
@@ -29,16 +29,19 @@ int main()
     {
         // Analog Input Programming: 00 (4 single ended inputs),
         // A/D Channel Number: 00(AIN0 )
-        wiringPiI2CWrite(i2c_fd, 0x40 | adcChannel);
-        preVal = wiringPiI2CRead(i2c_fd);                  // (0~255)
-        curVal = wiringPiI2CRead(i2c_fd);                  // ADC
-        printf("[%d] Previous value = %d, ", cnt, preVal); //
-        printf("Current value= %d, ", curVal);             //
-        // threshold , Bright
-        if (curVal < threshold)
-            printf("Bright!\n");
-        else
-            printf("Dark!\n");
+        for (int i = 0; i < 3; i++)
+        {
+            wiringPiI2CWrite(i2c_fd, 0x40 | adcChannel[i]);
+            curVal = wiringPiI2CRead(i2c_fd);               // ADC
+            printf("[%d] Current value = %d", cnt, curVal); //
+            //     // threshold , Bright
+            //     if (curVal < threshold)
+            //         printf("Bright!\n");
+            //     else
+            //         printf("Dark!\n");
+            printf("\n");
+        }
+        printf("\n");
         delay(500);
         cnt++;
     }
